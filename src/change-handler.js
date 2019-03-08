5067848725e7c644c1f5e4fde83b91d56e689de5
Derek <derekdenhartigh@gmail.com>
2019-03-08 14:27:48 -0500

    tests for howMuchIOweYou() and isPaymentSUfficient() passing

--- src/change-handler.js
+++ src/change-handler.js
@@ -5,12 +5,13 @@
  */
 
 // Variable declaration:
-let type, coin, change, cashTendered, changeReturn;
+let type, coin, change, cashTendered, changeReturn, quarters, dimes, nickels, pennies;
 class ChangeHandler {
 
     constructor(amountDue) {
         this.amountDue = amountDue;
         this.cashTendered = 0;
+        this.changeDue = 0;
     }
 
     /**
@@ -25,66 +26,86 @@ class ChangeHandler {
             case "nickle": this.cashTendered =+ 5; return this.cashTendered;
             case "dime": this.cashTendered =+ 10; return this.cashTendered;
             case "quarter": this.cashTendered =+ 25; return this.cashTendered;
-            // case "half-dollar": // Just me being extra
-            // case "half dollar": cashTendered =+ 50; return cashTendered;
             default: console.error(`${type} is not a valid coin!`);
         }
+        this.isPaymentSufficient();
     };
 
     /**
      * Returns true if enough coins have been inserted to at least meet the this.amountDue
      */
     isPaymentSufficient() {
-        if (this.cashTendered>this.amountDue){
+        if (this.cashTendered<this.amountDue){
             this.amountDue = this.amountDue-this.cashTendered;
-            console.log(`Insufficient funds, please insert ${this.amountDue} more cents to purchase item.`);
-            return this.amountDue;
+            console.log(`Insufficient funds, please insert ${this.amountDue} cents more to purchase item.`);
+            return false;
         }
-        if (this.cashTendered=this.amountDue){
+        else if (this.cashTendered===this.amountDue){
             this.amountDue = 0;
             console.log(`Huzzah! you have purchased your item, enjoy!`)
-            return this.amountDue;
+            return true;
         }
-        if (this.cashTendered>this.amountDue)
-            change = this.cashTendered-this.amountDue;
-            console.log(`Huzzah! you have overpaid for your item, dispensing item & ${change} cents`);
-            return change;
+        else if (this.cashTendered>this.amountDue)
+            this.changeDue = this.cashTendered-this.amountDue;  // I initially had this return this.changeDue, before I read the true/false req
+            console.log(`Huzzah! you have overpaid for your item, dispensing item & ${this.changeDue} cents`);
+            return true;
     };
-
+    howMuchIOweU(){ // I made this to modify amountDue
+    if (this.cashTendered<this.amountDue){
+        this.amountDue = this.amountDue-this.cashTendered;
+        return this.amountDue;
+        }
+    else if (this.cashTendered===this.amountDue || this.cashTendered>this.amountDue){
+        this.amountDue = 0;
+        return this.amountDue;
+        }
+    }
+    oopsIOverpaid(){  // made this to set changeDue value when overpaying
+        if (this.cashTendered>this.amountDue){
+            this.changeDue = this.cashTendered-this.amountDue;  // I initially had this return this.changeDue, before I read the true/false req
+            return this.changeDue; 
+        }
+    }
     giveChange() {
-        // TODO return the correct change in the following format...
-
-        let changeReturn = {
-            quarters: 0,
-            dimes: 0,
-            nickels: 0,
-            pennies: 0
+        if (this.isPaymentSufficient()===false){
+            this.insertCoin();
         }
-
-        if (change%25===0){
-            changeReturn.quarters = change/25;
+        else{
+            let changeReturn = {
+                quarters: 0,
+                dimes: 0,
+                nickels: 0,
+                pennies: 0
+            }
+            if (this.changeDue===0){
+                console.warn("I owe you nothing! Select another item or be gone Mortal");
+                return this.changeDue;
+            }
+            else if (this.changeDue%25===0){
+                changeReturn.quarters = this.changeDue/25; // change due is divisible by 25, dispenses (changeReturn.quarters)# of quaters
+            }
+            else if (this.changeDue%25===1){
+                changeReturn.quarters = math.floor(this.changeDue/25);
+                this.changeDue -= (math.floor(this.changeDue/25)*25); // takes all the quarters out, remaining change<25
+            }
+            else if (this.changeDue%10===0){
+                changeReturn.dimes = this.changeDue/10;
+            }
+            else if (this.changeDue%10===1){
+                changeReturn.dimes = math.floor(this.changeDue/10);
+                this.changeDue -= (math.floor(this.changeDue/10)*10); // change remaining < 10
+            }
+            else if (this.changeDue%5===0){
+                changeReturn.nickles = this.changeDue/5;
+            }
+            else if (this.changeDue%5===1){
+                changeReturn.nickles = math.floor(this.changeDue/5);
+                this.changeDue -= (math.floor(this.changeDue/5)*5);
+            }
+            else { // pennies is all thats left
+                changeReturn.pennies = this.changeDue;
+            };
+            return changeReturn;  // return the object with modified denomination counts.    
         }
-        else if (change%25===1){
-            changeReturn.quarters = math.floor(change/25);
-            change -= (math.floor(change/25)*25); // takes all the quarters out, remaining change<25
-        }
-        else if (change%10===0){
-            changeReturn.dimes = change/10;
-        }
-        else if (change%10===1){
-            changeReturn.dimes = math.floor(change/10);
-            change -= (math.floor(change/10)*10); // change remaining < 10
-        }
-        else if (change%5===0){
-            changeReturn.nickles = change/5;
-        }
-        else if (change%5===1){
-            changeReturn.nickles = math.floor(change/5);
-            change -= (math.floor(change/5)*5);
-        }
-        else { // pennies is all thats left
-            changeReturn.pennies = change;
-        };
-        return changeReturn;  // return the object with modified denomination counts.
     };
 };
--- tests/change-handler.spec.js
+++ tests/change-handler.spec.js
@@ -1,14 +1,62 @@
 // These test cases are all unfinished. We will finish them together.
-describe("tests for change-handler", function() {
+describe("tests for ChangeHandler class", function() {
 
-    // Set up a test below...
-    it("what do I write here?", function() {
+    // tests for insertCoin();
+    it("insertCoin() correctly modifies cashTendered", function() {
         // Remember, you can arrange, act, and assert...start small
         let changeHandler = new ChangeHandler();
         expect(changeHandler.insertCoin('penny')).toBe(1);
+        expect(changeHandler.insertCoin('pEnNy')).toBe(1);
         expect(changeHandler.insertCoin('nickle')).toBe(5);
         expect(changeHandler.insertCoin('dime')).toBe(10);
         expect(changeHandler.insertCoin('quarter')).toBe(25);
         expect(changeHandler.insertCoin('ducat')).toBe(undefined);
     });
+
+    it("checks isPaymentSufficient() to be set to true/false if you've paid enough to purchase item", function() {
+        let changeHandler = new ChangeHandler(100);  // arrange
+        changeHandler.cashTendered = 25; // act
+        expect(changeHandler.isPaymentSufficient()).toBe(false);// assert
+        
+        let changeHandler2 = new ChangeHandler(100);  // arrange
+        changeHandler2.cashTendered = 100; // act
+        expect(changeHandler2.isPaymentSufficient()).toBe(true); // assert
+    
+        let changeHandler3 = new ChangeHandler(100);  // arrange
+        changeHandler3.cashTendered = 125; // act
+        expect(changeHandler3.isPaymentSufficient()).toBe(true); // assert
+    });
+    it("checks howMuchIOweU() to see if it modifies this.change appropriately", function(){
+        let changeHandler = new ChangeHandler(100);  // arrange
+        changeHandler.cashTendered = 25; // act
+        changeHandler.howMuchIOweU();
+        expect(changeHandler.changeDue===(0));// assert
+        
+        let changeHandler2 = new ChangeHandler(100);  // arrange
+        changeHandler2.cashTendered = 100; // act
+        changeHandler.howMuchIOweU();
+        expect(changeHandler2.changeDue===(0)); // assert
+    
+        let changeHandler3 = new ChangeHandler(100);  // arrange
+        changeHandler3.cashTendered = 125; // act
+        changeHandler.howMuchIOweU();
+        expect(changeHandler3.changeDue===(25)); // assert
+    });
+    // it("giveChange returns the obj changeReturn with coins in optimal denominations (most quarters, least pennies possible)", function() {
+    //     let changeHandler = new ChangeHandler();
+    //     expect(changeHandler.giveChange(125).changeReturn.quarters).toBe(5);
+    // });
 });
+
+/*
+it("receiveAttackDamage removes health", function() {
+// Arrange
+var player = new Character({
+health: 20
+});
+// Act
+player.receiveAttackDamage(5);
+// Assert
+expect(player.health).toBe(15);
+});
+*/
